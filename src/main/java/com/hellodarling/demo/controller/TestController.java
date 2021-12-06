3dd0da700822de66a343de715699e32c7aedb258
yulin <yulin.chen@thoughtworks.com>
2021-12-06 20:28:10 +0800

    [yulin] feat: add a necessary annotation '@RestController' in `TestController.java`

--- src/main/java/com/hellodarling/demo/controller/TestController.java
+++ src/main/java/com/hellodarling/demo/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.hellodarling.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "how are you?";
+        return "hello";
     }
 }
